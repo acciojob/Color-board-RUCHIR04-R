f9658561638376ce135946eaa726a87090f2341b
RUCHIR04-R <ruchirgawture@gmail.com>
2025-02-25 09:46:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,20 +7,11 @@ document.addEventListener("DOMContentLoaded", function () {
         container.appendChild(square);
 
         square.addEventListener('mouseover', function () {
-            square.style.backgroundColor = getRandomColor();
+            square.style.backgroundColor = 'rgb(29, 29, 29)';
             setTimeout(function () {
                 square.style.backgroundColor = '#fff';
             }, 1000);
         });
-    }
-
-    function getRandomColor() {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
     }
 });
 
