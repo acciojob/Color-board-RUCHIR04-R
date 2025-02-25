466faf9e3335eca2a7b0f568ba725961911ec429
RUCHIR04-R <ruchirgawture@gmail.com>
2025-02-25 09:37:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-//your JS code here. If required.
-// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.querySelector('.container');
 
-const container = document.querySelector('.container');
-for (let i = 0; i < 800; i++) {
-    const div = document.createElement('div');
-    div.classList.add('square');
-    container.appendChild(div);
-}
+    for (let i = 0; i < 800; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+
+        square.addEventListener('mouseover', function () {
+            square.style.backgroundColor = getRandomColor();
+            setTimeout(function () {
+                square.style.backgroundColor = '#fff';
+            }, 1000);
+        });
+    }
+
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+});
